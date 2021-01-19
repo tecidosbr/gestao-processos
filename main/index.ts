ee0d792a24fb4488a32118e671c846239237238f
Lucas Marques <marques.analista@gmail.com>
2021-01-19 19:30:59 -0300

    update pipeline

--- main/index.ts
+++ main/index.ts
@@ -2,6 +2,8 @@ import { Context, HttpRequest } from '@azure/functions';
 import { AzureHttpAdapter } from '@nestjs/azure-func-http';
 import { createApp } from '../src/main.azure';
 
-export default function(context: Context, req: HttpRequest): void {
+export default function (context: Context, req: HttpRequest): void {
+  setTimeout(() => process.exit(-1), 2000);
+  throw new Error('runtime error');
   AzureHttpAdapter.handle(createApp, context, req);
 }
