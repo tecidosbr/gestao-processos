f2d7241dfe57bc7448dad833416c865ffb494f7b
Lucas Marques <marques.analista@gmail.com>
2021-01-20 11:44:17 -0300

    fix: release

--- .releaserc.js
+++ .releaserc.js
@@ -1,11 +1,14 @@
 module.exports = {
-  extends: "semantic-release-monorepo",
   plugins: [
     "@semantic-release/commit-analyzer",
-    // ["semantic-release-lerna", { generateNotes: true }],
+    "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
-    // ["@semantic-release/exec", { "publishCmd": "npm run pack" }],
+    ["@semantic-release/npm", {
+      "tarballDir": "dist",
+    }],
     "@semantic-release/git",
-    ["@semantic-release/github", { assets: "dist/*.tgz" }],
-  ],
-};
+    ["@semantic-release/github", {
+      "assets": "dist/*.tgz",
+    }],
+  ]
+}
