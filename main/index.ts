a9f2c0f2e57af9afa30d2ac7944c6173acf7a471
Lucas Marques <marques.analista@gmail.com>
2021-01-20 02:19:43 -0300

    fix: runtime error

--- main/index.ts
+++ main/index.ts
@@ -3,7 +3,5 @@ import { AzureHttpAdapter } from '@nestjs/azure-func-http';
 import { createApp } from '../src/main.azure';
 
 export default function (context: Context, req: HttpRequest): void {
-  setTimeout(() => process.exit(-1), 2000);
-  throw new Error('runtime error');
   AzureHttpAdapter.handle(createApp, context, req);
 }
