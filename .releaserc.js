6a29a729c63fa731ff3b80d677f0e34899fa1417
Lucas Marques <marques.analista@gmail.com>
2021-01-20 16:55:12 -0300

    fix: releaserc

--- .releaserc.js
+++ .releaserc.js
@@ -7,10 +7,10 @@ module.exports = {
       "publishCmd": "lerna version --conventional-commits --yes"
     }],
     ["@semantic-release/exec", {
-      "publishCmd": "npx shx rm -rf dist && npx shx mkdir dist"
+      "publishCmd": "shx rm -rf dist && shx mkdir dist"
     }],
     ["@semantic-release/exec", {
-      "publishCmd": "npx lerna exec -- node ../../node_modules/npm-pack-all --output ../../dist/"
+      "publishCmd": "lerna exec -- node ../../node_modules/npm-pack-all --output ../../dist/"
     }],
     "@semantic-release/git",
     ["@semantic-release/github", {
