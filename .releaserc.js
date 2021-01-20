9a6c26abf2eeb73542139b757f09687e6e511733
Lucas Marques <marques.analista@gmail.com>
2021-01-20 17:18:39 -0300

    fix: generate zip archives

--- .releaserc.js
+++ .releaserc.js
@@ -12,9 +12,12 @@ module.exports = {
     ["@semantic-release/exec", {
       "publishCmd": "lerna exec -- node ../../node_modules/npm-pack-all --output ../../dist/"
     }],
+    ["@semantic-release/exec", {
+      "publishCmd": "tar2zip dist/*.tgz"
+    }],
     "@semantic-release/git",
     ["@semantic-release/github", {
-      "assets": "packages/*/*.tgz",
+      "assets": "dist/*",
     }],
   ]
 }
