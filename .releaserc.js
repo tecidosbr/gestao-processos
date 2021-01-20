314734d0914552686cd009667e98e2778cecda2c
Lucas Marques <marques.analista@gmail.com>
2021-01-19 23:06:57 -0300

    use ghactions as devops pipeline

---
+++ .releaserc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/npm',
+      {
+        tarballDir: 'dist',
+      },
+    ],
+    '@semantic-release/git',
+    [
+      '@semantic-release/github',
+      {
+        assets: 'dist/*.tgz',
+      },
+    ],
+  ],
+};
