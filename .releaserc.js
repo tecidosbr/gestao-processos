141396232269b01f150da0b20caa7d70d5e00f80
lmarqs <lucas.paula@wipro.com>
2021-01-20 23:09:42 -0300

    fix: command

--- .releaserc.js
+++ .releaserc.js
@@ -6,16 +6,16 @@ module.exports = {
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     ["@semantic-release/exec", {
-      "publishCmd": "lerna version ${nextRelease.version} --no-git-tag-version --yes"
+      "prepareCmd": "lerna version ${nextRelease.version} --no-git-tag-version --yes"
     }],
     ["@semantic-release/exec", {
-      "publishCmd": "shx rm -rf release && shx mkdir release"
+      "prepareCmd": "shx rm -rf release && shx mkdir release"
     }],
     ["@semantic-release/exec", {
-      "publishCmd": "lerna exec -- node ../../node_modules/npm-pack-all --output ../../release/"
+      "prepareCmd": "lerna exec -- node ../../node_modules/npm-pack-all --output ../../release/"
     }],
     ["@semantic-release/exec", {
-      "publishCmd": "tar2zip release/*.tgz"
+      "prepareCmd": "tar2zip release/*.tgz"
     }],
     "@semantic-release/git",
     ["@semantic-release/github", {
