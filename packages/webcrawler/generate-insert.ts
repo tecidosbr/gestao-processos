d5afcebf7be105f96096467e60e4b5c1e0fe14b4
lmarqs <lucas.paula@wipro.com>
2021-02-01 17:56:52 -0300

    fix insert script

--- packages/webcrawler/generate-insert.ts
+++ packages/webcrawler/generate-insert.ts
@@ -2,5 +2,5 @@ import filenamify from 'filenamify/filenamify';
 import normas from './out/normas.json';
 
 for (const n of normas) {
-    console.log(`INSERT INTO norma VALUES ('${n.id}', '${n.titulo}', '${n.objetivo}', '${n.ics}', '${n.palavrasChave}');`);
+    console.log(`INSERT INTO normas VALUES ('${n.id}', '${n.titulo}', '${n.objetivo}', '${n.ics}', '${n.palavrasChave}');`);
 }
