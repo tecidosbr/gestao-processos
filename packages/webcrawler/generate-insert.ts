ebc566a952bdf1645c30543ba0477f19b3229543
lmarqs <lucas.paula@wipro.com>
2021-02-01 19:37:25 -0300

    remove webcrawler from main branch

--- packages/webcrawler/generate-insert.ts
+++
@@ -1,8 +0,0 @@
-import filenamify from 'filenamify';
-import normas from './out/normas.json';
-
-for (const [id, n] of Object.entries(normas)) {
-    console.log(`
-    INSERT INTO normas (id, codigo, titulo, objetivo, ics, palavrasChave, filename)
-    VALUES (${id + 1}, '${n.id}', '${n.titulo}', '${n.objetivo}', '${n.ics}', '${JSON.stringify(n.palavrasChave)}', '${filenamify(n.id, { replacement: '_' })}.pdf');`);
-}
