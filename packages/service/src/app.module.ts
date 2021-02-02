02fa96d7d2cf02b5b49ad62e18e564dc330f1b6c
lmarqs <lucas.paula@wipro.com>
2021-02-02 18:15:28 -0300

    fix: add health check

--- packages/service/src/app.module.ts
+++ packages/service/src/app.module.ts
@@ -1,9 +1,10 @@
 import { InMemoryDBModule } from '@nestjs-addons/in-memory-db';
 import { Module } from '@nestjs/common';
+import { TerminusModule } from '@nestjs/terminus';
 import { controllers } from './controllers';
 import { providers } from './service';
 @Module({
-  imports: [InMemoryDBModule.forRoot({})],
+  imports: [InMemoryDBModule.forRoot({}), TerminusModule],
   controllers,
   providers,
 })
--- packages/service/src/controllers/index.ts
+++ packages/service/src/controllers/index.ts
@@ -1,10 +1,12 @@
 import { ComiteController } from './comite.controller';
+import { HealthController } from './health.controller';
 import { IcsController } from './ics.controller';
 import { NormaController } from './norma.controller';
 import { OrganismoController } from './organismo.controller';
 
 export const controllers = [
   ComiteController,
+  HealthController,
   IcsController,
   NormaController,
   OrganismoController,
