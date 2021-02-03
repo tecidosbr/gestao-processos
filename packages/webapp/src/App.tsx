f08cd76e35318a7542c3fd066be7a0e3810dcc0e
lmarqs <lucas.paula@wipro.com>
2021-02-02 23:02:18 -0300

    feat: add routing

--- packages/webapp/src/App.tsx
+++ packages/webapp/src/App.tsx
@@ -1,13 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import { AuthenticationContextProvider } from './auth';
 import { Nav } from './Nav';
+import { Body } from './Body';
 
 export const App: React.FC = () => {
   return (
     <AuthenticationContextProvider>
-      <div className="container-fluid">
+      <Router>
         <Nav />
-      </div>
+        <Body />
+      </Router>
     </AuthenticationContextProvider>
   );
+}
+
+declare global {
+        namespace JSX {
+        interface IntrinsicElements {
+        'gestao-contratos': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+          'gestao-normas': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+          'gestao-processos': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+      }
+  }
 }
---
+++ packages/webapp/src/Body.tsx
@@ -0,0 +1,29 @@
+import React, { useContext, useEffect } from 'react';
+import { Switch, Route, withRouter } from "react-router-dom";
+import { AuthenticationContext } from './auth';
+
+export const Body = withRouter(({ history }) => {
+    const authCtx = useContext(AuthenticationContext);
+
+    useEffect(() => {
+        if (!authCtx.account) {
+            history.push('/');
+        }
+    }, [authCtx.account]);
+
+    return (
+        <div className="container-fluid">
+            <Switch>
+                <Route path="/gestao-normas">
+                    <gestao-normas />
+                </Route>
+                <Route path="/gestao-processos">
+                    <gestao-processos />
+                </Route>
+                <Route path="/gestao-contratos">
+                    <gestao-contratos />
+                </Route>
+            </Switch>
+        </div>
+    );
+});
