734e9bcadcdac3333ae197f3a6346e150c8c9a9f
lmarqs <lucas.paula@wipro.com>
2021-02-03 09:10:27 -0300

    fix: remove unused vars

--- packages/webapp/src/App.tsx
+++ packages/webapp/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import { AuthenticationContextProvider } from './auth';
 import { Nav } from './Nav';
