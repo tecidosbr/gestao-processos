9b62549ebfb6abb49f7801558aaf59ab229e6c17
lmarqs <lucas.paula@wipro.com>
2021-02-02 22:59:03 -0300

    feat: enhance login

--- packages/webapp/src/Nav.tsx
+++ packages/webapp/src/Nav.tsx
@@ -1,20 +1,50 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { AuthenticationContext } from "./auth";
 
 export const Nav: React.FC = () => {
     const ctx = React.useContext(AuthenticationContext);
+    const [showItems, setShowItems] = React.useState<boolean>(false);
+    const toggleShowItems = React.useCallback(() => setShowItems(!showItems), [showItems]);
 
     if (!ctx.account) {
-        return <LogInButton />
+        return (
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <LogInButton />
+                    <div className="collapse navbar-collapse" />
+                </div>
+            </nav>
+        );
     }
 
-    console.log(ctx.account);
-
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
                 <LogOutButton />
-                {ctx.account?.name}
+                <div className="navbar-brand mx-3">Olá, {ctx.account.name}</div>
+                <button className="navbar-toggler" type="button" onClick={toggleShowItems}>
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className={`collapse navbar-collapse ${showItems ? 'show' : ''}`}>
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <Link to="/gestao-contratos">
+                                <span className="nav-link">Gestão de Contratos</span>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/gestao-normas">
+                                <span className="nav-link">Gestão de Normas</span>
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to="/gestao-processos">
+                                <span className="nav-link">Gestão de Processos</span>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     )
@@ -25,7 +55,7 @@ const LogInButton: React.FC = () => {
     const ctx = React.useContext(AuthenticationContext);
     const logIn = React.useCallback(() => ctx.login(), []);
     return (
-        <button onClick={logIn}>Log in</button>
+        <button className="btn btn-primary" onClick={logIn}>Log&nbsp;in</button>
     );
 };
 
@@ -33,6 +63,6 @@ const LogOutButton: React.FC = () => {
     const ctx = React.useContext(AuthenticationContext);
     const logOut = React.useCallback(() => ctx.logout(), []);
     return (
-        <button onClick={logOut}>Log out</button>
+        <button className="btn btn-outline-primary" onClick={logOut}>Log&nbsp;out</button>
     );
 };
