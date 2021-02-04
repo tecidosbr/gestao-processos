72e9dae476c114dd7e56419250aa504b96cf26e2
lmarqs <lucas.paula@wipro.com>
2021-02-04 07:36:03 -0300

    fix: enhance production dependencies

--- packages/service/src/main.azure.ts
+++ packages/service/src/main.azure.ts
@@ -1,12 +1,10 @@
 import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { bootstrap } from './bootstrap';
 
 export async function createApp(): Promise<INestApplication> {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  await bootstrap(app);
   await app.init();
   return app;
 }
--- packages/service/src/main.ts
+++ packages/service/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { bootstrap } from './bootstrap';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function createApp() {
   const app = await NestFactory.create(AppModule);
-  await bootstrap(app);
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Gestão Contratos')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(5000);
 }
 createApp();
